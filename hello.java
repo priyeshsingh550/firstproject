5424941aa574aabefb06cfbaaf03ebf757b44146
priyeshsingh550 <86961281+priyeshsingh550@users.noreply.github.com>
2021-07-07 19:55:20 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello Java this is priyesh first jenkin file created");  
+     System.out.println("Hello Java this is priyesh first jenkin file with the poll scm created");  
     }  
 }  
