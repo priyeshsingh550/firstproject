408bd3b399ae735a085dfd0d7564384be30d9ec9
priyeshsingh550 <86961281+priyeshsingh550@users.noreply.github.com>
2021-08-17 09:59:59 +0530

    Update helloworld.java
    pipeline {
        agent {
        stages {
         stage ('clone') { 
             steps{
    
            stage('build') {
                steps {
                    sh 'mvn --version'
                }
            }
        }
    }

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,1 @@
-class Simple{  
-    public static void main(String args[]){  
-     System.out.println("Hello Java this is priyesh first jenkin file with the poll scm can we created after three minutes it will updtae");  
-    }  
-}  
+
